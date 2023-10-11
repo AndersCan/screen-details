b2f6245fc1b4a1be145efc91ea97407c7bb9ff6a
Anders Olav Candasamy <anders.can1@gmail.com>
2023-10-11 14:21:40 +0200

    fix: base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: "/screen-tools/",
   server: {
     https: true,
   },
